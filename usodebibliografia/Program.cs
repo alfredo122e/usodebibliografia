85fb11a4b074b64a01f2821129712c0d5800d39a
Alfredo Lainez <zelayalainea@gmail.com>
2021-08-19 17:17:44 -0600

    Agregar archivos de proyecto.

---
+++ usodebibliografia/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using usodeejerciciodebiblioteca;
+
+namespace usodebibliografia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //agragamos la clase 
+            Class1.SortedListCollection();
+        }
+    }
+}
